9a5aea374b9107a98837d9cacc40638a1e084c60
Sanders <sndrs90@gmail.com>
2021-11-29 11:04:04 +0300

    Fill parseSandwichJson method in JsonUtils

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,49 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        Sandwich sandwich = new Sandwich();
+
+        JSONObject sandwichJson = new JSONObject(json);
+        JSONObject name = sandwichJson.getJSONObject("name");
+
+        //JSONObject mainNameJson = name.getJSONObject("mainName");
+        String mainNameString = name.getString("mainName");
+        sandwich.setMainName(mainNameString);
+
+        JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+        List<String> alsoKnownList = new ArrayList<String>();
+        for(int i = 0; i < alsoKnownAsArray.length(); i++){
+            alsoKnownList.add(alsoKnownAsArray.getString(i));
+        }
+        sandwich.setAlsoKnownAs(alsoKnownList);
+
+        String placeOfOriginString = sandwichJson.getString("placeOfOrigin");
+        sandwich.setPlaceOfOrigin(placeOfOriginString);
+
+        String descriptionString = sandwichJson.getString("description");
+        sandwich.setDescription(descriptionString);
+
+        String imageString = sandwichJson.getString("image");
+        sandwich.setPlaceOfOrigin(placeOfOriginString);
+
+        JSONArray ingredientsArray = sandwichJson.getJSONArray("ingredients");
+        List<String> ingredientsList = new ArrayList<String>();
+        for(int i = 0; i < ingredientsArray.length(); i++){
+            ingredientsList.add(ingredientsArray.getString(i));
+        }
+        sandwich.setIngredients(ingredientsList);
+
+
+        return sandwich;
     }
 }
